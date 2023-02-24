99fbfb6d1f4ff4d209126d123297ba6d10643324
jaleyerdelen <jaleyerdelen@gmail.com>
2023-02-24 13:47:39 +0300

    create serializer

---
+++ profiles/api/serializers.py
@@ -0,0 +1,21 @@
+from profiles.models import Profile, ProfileStatus
+from rest_framework import serializers
+
+class ProfileSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(read_only=True)
+    photo = serializers.ImageField(read_only=True)
+
+    class Meta:
+        model = Profile
+        fields = "__all__"
+
+class ProfilePhotoSerializer(serializers.ModelSerial):
+    model = Profile
+    fields = ["photo"]
+
+class ProfileStatusSerializer(serializers.ModelSerializer):
+    user_profile = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = ProfileStatus
+        fields = "__all__"
--- profiles/signals.py
+++ profiles/signals.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from profiles.models import Profile
+from profiles.models import Profile, ProfileStatus
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -8,3 +8,11 @@ def create_profile(sender, instance, created, **kwargs):
     print(instance.username, "___Created: ", created)
     if created:
         Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=Profile)
+def create_first_status_message(sender, instance, created, **kwargs):
+    if created:
+        ProfileStatus.objects.create(
+            user_profile = instance,
+            status_message = f"{instance.user.username} joined"
+        )
