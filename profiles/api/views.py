1444ac4406a6a1e2d2348b2a52e1075e751cd54c
jaleyerdelen <jaleyerdelen@gmail.com>
2023-03-07 17:53:16 +0300

    GenericViewSet and Mixins

--- profiles/api/views.py
+++ profiles/api/views.py
@@ -2,9 +2,17 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from profiles.models import Profile
 from profiles.api.serializers import ProfileSerializer
-from rest_framework.viewsets import ReadOnlyModelViewSet
+#from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.viewsets import GenericViewSet
+from rest_framework import mixins
+from profiles.api.permissions import OwnProfileOrReadOnly
 
-class ProfileViewSet(ReadOnlyModelViewSet):
+class ProfileViewSet(
+                mixins.ListModelMixin,
+                mixins.RetrieveModelMixin,
+                mixins.UpdateModelMixin,
+                GenericViewSet):
+
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, OwnProfileOrReadOnly]
